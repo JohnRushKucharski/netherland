d3b2b0abdcfb17e94d36245ba24022ec154b8895
JohnRushKucharski <johnkucharski@gmail.com>
2023-12-24 21:53:22 -1000

    added live unit test, minimal validation & readme

--- src/live.py
+++ src/live.py
@@ -8,6 +8,7 @@ import numpy as np
 
 from src.constants import Constants
 from src.stock import Tag, Measurement, conversion_builder
+from src.validators import non_negative_attribute, ascending_non_negative_attribute
 
 # Aliases
 Depths: TypeAlias = tuple[float, float]
@@ -16,7 +17,7 @@ Turnover: TypeAlias = tuple[float, float, float]
 # pylint: disable=invalid-name #underscore typealias
 F_Distribution: TypeAlias = Callable[[float], float]
 F_Integration: TypeAlias = Callable[[Depths], float]
-F_Turnover: TypeAlias = Callable[[Depths, float], Turnover]
+F_Turnover: TypeAlias = Callable[[float], Turnover]
 
 def distribution_builder(constants: Constants) -> Callable[[float], F_Distribution]:
     '''
@@ -29,10 +30,12 @@ def distribution_builder(constants: Constants) -> Callable[[float], F_Distributi
         '''
         Returns a fully parameterized biomass distribution function.
         '''
+        non_negative_attribute('top', top)
         def fx(depth: float) -> float:
             '''
             Computes biomass [in g] at a given depth [in cm].
             '''
+            non_negative_attribute('depth', depth)
             if depth > root_depth:
                 return 0
             return top * np.exp(-k1 * depth) * surface_area
@@ -52,7 +55,8 @@ def integration_builder(constants: Constants) -> Callable[[F_Distribution], F_In
             '''
             Returns the integral of the biomass [in g] distribution function across depths [in cm].
             '''
-            return fx(depths[1]) - fx(depths[0]) / -k1
+            ascending_non_negative_attribute('depths', depths)
+            return (fx(depths[1]) - fx(depths[0])) / -k1
         return integrate
     return partial
 
@@ -60,28 +64,26 @@ def turnover_builder(constants: Constants) -> Callable[[F_Integration], F_Turnov
     '''
     Returns partially parameterized biomass turnover function.
     '''
+    k2 = constants.k2
     fc = constants.fc
     fl = constants.fl
-    def partial(fx: F_Integration) -> F_Turnover:
+    def turnover(weight: float, years: float) -> Turnover:
         '''
-        Returns fully parameterized biomass turnover function.
-        '''
-        def turnover(depths: Depths, years: float) -> Turnover:
-            '''
-            Computes biomass [in g] removed due to turnover [in dmls/yr].
+        Computes biomass [in g] removed due to turnover [in dmls/yr].
             
-            Returns:
-                Tuple[float, float, float]: refractory, labile, inorganic [in g].
-            '''
-            mass = fx(depths)
-            return (
-                fl * mass * years,
-                fc * mass * years,
-                0.0 # by assumption in Morris & Bowden (1986).
-            )
-        return turnover
-    return partial
+        Returns:
+            Tuple[float, float, float]: refractory, labile, inorganic [in g].
+        '''
+        non_negative_attribute('weight', weight)
+        non_negative_attribute('years', years)
+        return (
+            weight * k2 * fl * years,
+            weight * k2 * fc * years,
+            0.0 # by assumption in Morris & Bowden (1986).
+        )
+    return turnover
 
+# TODO: is depth in erosion and burial the layer depths, if so should I check that the erosion or deposition is not less depth.
 def erosion_builder(constants: Constants) -> Callable[[F_Integration], F_Turnover]:
     '''
     Returns partially parameterized function removing biomass [in g] due to erosion [in cm].
@@ -95,11 +97,17 @@ def erosion_builder(constants: Constants) -> Callable[[F_Integration], F_Turnove
             '''
             Computes biomass [in g] removed due to erosion [in cm].
             
+            Args:
+                depths (Depths): top and bottom depths [in cm] of the layer.
+                erosion (float): erosion [in cm] over the timestep. Must be non-negative.
+            
             Returns:
                 Tuple[float, float, float]: refractory, labile, inorganic [in g].
             '''
+            ascending_non_negative_attribute('depths', depths)
+            non_negative_attribute('erosion', erosion)
             mass = fx((depths[0], depths[0] + erosion))
-            return (mass * fl, mass * fc, mass * k3)
+            return (mass * (1 - k3) * fl, mass * (1 - k3) * fc, mass * k3)
         return erosion
     return partial
 
@@ -120,6 +128,8 @@ def burial_builder(constants: Constants) -> Callable[[F_Integration], F_Turnover
             Returns:
                 Tuple[float, float, float]: refractory, labile, inorganic [in g].
             '''
+            ascending_non_negative_attribute('depths', depths)
+            non_negative_attribute('deposition', deposition)
             def is_in_live_zone(depth_delta: float = 0):
                 return depths[1] + depth_delta <= root_depth
             if is_in_live_zone() and not is_in_live_zone(deposition):
@@ -142,9 +152,9 @@ class PartialTools:
     def __init__(self, constants: Constants):
         self.partial_burial = burial_builder(constants)
         self.partial_erosion = erosion_builder(constants)
-        self.partial_turnover = turnover_builder(constants)
         self.partial_integration = integration_builder(constants)
         self.partial_distribution = distribution_builder(constants)
+        self.turnover = turnover_builder(constants)
         self.converter = conversion_builder(constants)
 
 class Tools:
@@ -153,9 +163,9 @@ class Tools:
     '''
     def __init__(self, partial_tools: PartialTools, integration: F_Integration):
         self.converter = partial_tools.converter
+        self.turnover = partial_tools.turnover
         self.burial = partial_tools.partial_burial(integration)
         self.erosion = partial_tools.partial_erosion(integration)
-        self.turnover = partial_tools.partial_turnover(integration)
 
 @dataclass(frozen=True)
 class Biomass:
@@ -165,6 +175,10 @@ class Biomass:
     tag: Tag = Tag.BIOMASS
     measurement: Measurement = Measurement.WEIGHT
 
+    @property
+    def tag(self) -> Tag:
+        '''Returns the tag of the biomass.'''
+        return Tag.BIOMASS
     @property
     def length(self) -> float:
         '''Returns the length [in cm] of the biomass.'''
@@ -177,6 +191,22 @@ class Biomass:
         if self.measurement == Measurement.WEIGHT:
             return self.val
         return self.fxs.converter(self.val, self.tag, Measurement.WEIGHT)
+
+    def transfers(self, deposition: float, depths: Depths, yrs: float) -> tuple[Turnover, Turnover]:
+        '''
+        Computes turnover and burial or erosion [in g] of biomass over timeperiod [in yrs].
+        
+        By assumption, based on Morris & Bowden (1986) preceeding equation 10, "root uptake &
+        root death are equivalent" thus turnover is instantaneous replaced in equal quantities
+        by new living biomass. Therefore turnover is not substracted from the biomass stock
+        before accounting for burial or erosion. As a result turnover is a net gain for the cohert.
+        '''
+        if deposition > 0:
+            return (tuple(np.sum(self.fxs.turnover(self.weight, yrs), self.fxs.burial(depths, deposition))),  # pylint: disable=line-too-long
+                    (0.0, 0.0 , 0.0)) # no losses from system.
+        else:
+            return (self.fxs.turnover(self.weight, yrs),
+                    self.fxs.erosion(depths, -deposition)) # loss from system
 
 def factory(top: float, depths: Depths, tools: PartialTools) -> Biomass:
     '''
--- tests/live_test.py
+++ tests/live_test.py
@@ -4,7 +4,8 @@ Tests the live.py module.
 import unittest
 
 from src.constants import import_file
-from src.live import distribution_builder
+from src.live import (distribution_builder, integration_builder,
+                      turnover_builder, erosion_builder, burial_builder)
 
 test_constants = import_file()[0]
 
@@ -12,24 +13,329 @@ class TestDistributionBuilder(unittest.TestCase):
     '''
     Tests the distribution_builder function.
     '''
-    def tests_distribution_builder_returns_callable(self):
+    def test_distribution_builder_returns_callable(self):
         '''
         Tests the distribution_builder function returns a callable.
         '''
         self.assertTrue(callable(distribution_builder(test_constants)))
-    def tests_distribution_builder_partial_returns_callable(self):
+
+class TestPartialDistribution(unittest.TestCase):
+    '''
+    Tests the partial distribution function.
+    
+    A closure that is returned by the distribution_builder function.
+    '''
+    def test_partial_distribution_returns_callable(self):
         '''
         Tests the distribution_builder function returns a function.
         '''
         self.assertTrue(callable(distribution_builder(test_constants)(1.0)))
+    def test_partial_distribution_top_less_than_0_raises_value_error(self):
+        '''
+        Depth less than 0 raises ValueError.
+        '''
+        f = distribution_builder(test_constants)
+        with self.assertRaises(ValueError):
+            f(-1.0)
 
 class TestDistribution(unittest.TestCase):
     '''
     Tests the distribution function.
+    
+    A closure that is returned by the distribution_builder->partial_distribution function.
     '''
-    def tests_distribution_returns_tuple(self):
+    def test_distribution_returns_float(self):
         '''
-        Tests the distribution function returns a tuple.
+        Tests the distribution function returns a float.
         '''
         dist = distribution_builder(test_constants)(1.0)
         self.assertTrue(isinstance(dist(1.0), float))
+    def test_distribution_depth_less_than_0_raises_value_error(self):
+        '''
+        Depth less than 0 raises ValueError.
+        '''
+        f = distribution_builder(test_constants)(1.0)
+        with self.assertRaises(ValueError):
+            f(-1.0)
+    def test_distribution_depth_equal_to_0_returns_top(self):
+        '''
+        Depth equal to 0 returns top.
+        '''
+        f = distribution_builder(test_constants)(1.0)
+        self.assertEqual(f(depth=0.0), 1.0)
+    def test_distribution_depth_equal_to_rd_returns_0(self):
+        '''
+        Depth equal to root depth returns near 0.
+        '''
+        f = distribution_builder(test_constants)(1.0)
+        self.assertAlmostEqual(f(depth=test_constants.rd), 0.04978706836786395)
+    def test_distribution_depth_gt_rd_returns_0(self):
+        '''
+        Depth greater than root depth returns 0.
+        '''
+        f = distribution_builder(test_constants)(1.0)
+        self.assertAlmostEqual(f(test_constants.rd + 1), 0)
+    def test_distribution_no_biomass_at_top_returns_0_at_0_depth(self):
+        '''
+        No biomass at surface returns 0 at surface.
+        '''
+        f = distribution_builder(test_constants)(0.0)
+        self.assertAlmostEqual(f(depth=0.0), 0.0)
+    def test_distribution_no_biomass_at_top_returns_0_at_1_depth(self):
+        '''
+        No biomass at surface returns 0 at surface.
+        '''
+        f = distribution_builder(test_constants)(0.0)
+        self.assertAlmostEqual(f(depth=1.0), 0.0)
+    def test_biomass_at_depth_1_biomass_returns_approx_0_9_at_1_depth(self):
+        '''
+        1 biomass at surface returns 0.9 at 1 depth, with k1=0.1.
+        
+        Note: This is less than the biomass at the surface, because calculus :).
+        '''
+        f = distribution_builder(test_constants)(1.0)
+        self.assertAlmostEqual(f(depth=1.0), 0.9, places=2)
+
+class TestIntegrationBuilder(unittest.TestCase):
+    '''
+    Tests the integration_builder function.
+    '''
+    def test_integration_builder_returns_callable(self):
+        '''
+        Tests the integration_builder function returns a callable.
+        '''
+        self.assertTrue(callable(integration_builder(test_constants)))
+
+class TestPartialIntegration(unittest.TestCase):
+    '''
+    Tests the partial integration function.
+    
+    A closure that is returned by the integration_builder function.
+    '''
+    def test_partial_integration_returns_callable(self):
+        '''
+        Tests the integration_builder function returns a function.
+        '''
+        self.assertTrue(callable(integration_builder(test_constants)(1.0)))
+
+class TestIntegration(unittest.TestCase):
+    '''
+    Tests the integration function.
+    
+    A closure that is returned by the integration_builder->partial_integration function.
+    '''
+    def test_integration_returns_float(self):
+        '''
+        Tests the integration function returns a float.
+        '''
+        dist = distribution_builder(test_constants)(1.0)
+        f = integration_builder(test_constants)(dist)
+        self.assertTrue(isinstance(f((0,1)), float))
+    def test_integration_depths_less_than_0_raises_value_error(self):
+        '''
+        Depth less than 0 raises ValueError.
+        '''
+        dist = distribution_builder(test_constants)(1.0)
+        f = integration_builder(test_constants)(dist)
+        with self.assertRaises(ValueError):
+            f((0, -1.0))
+    def test_integration_ascending_depths_raises_value_error(self):
+        '''
+        Ascending depths raises ValueError.
+        '''
+        dist = distribution_builder(test_constants)(1.0)
+        f = integration_builder(test_constants)(dist)
+        with self.assertRaises(ValueError):
+            f((1.0, 0))
+    def test_integration_depths_equal_to_0_returns_0(self):
+        '''
+        Depths equal to 0 returns 0.
+        '''
+        dist = distribution_builder(test_constants)(1.0)
+        f = integration_builder(test_constants)(dist)
+        self.assertAlmostEqual(f((0, 0)), 0.0)
+
+    def test_integration_first_increment_between_0_and_top(self):
+        '''
+        First increment of integration is between 0 and top.
+        '''
+        top = 1.0
+        dist = distribution_builder(test_constants)(top)
+        val = integration_builder(test_constants)(dist)((0.0, 1.0))
+        self.assertLess(val, 1.0)
+        self.assertGreater(val, 0.0)
+
+    def test_expose_default_biomass_over_rd_per_unit_at_top(self):
+        '''
+        Exposes default biomass over rd per unit at top.
+        
+        Each unit of biomass as surface given Morris default
+        parameters for k, and rd produces ~9.5 g of biomass per cm2 cell.
+        '''
+        top = 1.0
+        dist = distribution_builder(test_constants)(top)
+        val = integration_builder(test_constants)(dist)((0.0, test_constants.rd))
+        self.assertAlmostEqual(val, 9.50, 2)
+
+class TestTurnoverBuilder(unittest.TestCase):
+    '''
+    Tests the turnover_builder function.
+    '''
+    def test_turnover_builder_returns_callable(self):
+        '''
+        Tests the turnover_builder function returns a callable.
+        '''
+        self.assertTrue(callable(turnover_builder(test_constants)))
+
+class TestTurnover(unittest.TestCase):
+    '''
+    Tests the turnover function.
+    '''
+    def test_turnover_returns_tuple(self):
+        '''
+        Tests the turnover function returns a tuple.
+        '''
+        turnover = turnover_builder(test_constants)
+        self.assertTrue(isinstance(turnover(weight=1.0, years=1.0), tuple))
+    def test_turnover_negative_weight_raises_value_error(self):
+        '''
+        Negative weight raises ValueError.
+        '''
+        turnover = turnover_builder(test_constants)
+        with self.assertRaises(ValueError):
+            turnover(weight=-1.0, years=1.0)
+    def test_turnover_negative_years_raises_value_error(self):
+        '''
+        Negative years raises ValueError.
+        '''
+        turnover = turnover_builder(test_constants)
+        with self.assertRaises(ValueError):
+            turnover(weight=1.0, years=-1.0)
+    def test_turnover_zero_weight_returns_zeros(self):
+        '''
+        Zero weight returns zeros.
+        '''
+        turnover = turnover_builder(test_constants)
+        self.assertEqual(turnover(weight=0.0, years=1.0), (0.0, 0.0, 0.0))
+    def test_turnover_zero_years_returns_zeros(self):
+        '''
+        Zero years returns zeros.
+        '''
+        turnover = turnover_builder(test_constants)
+        self.assertEqual(turnover(weight=1.0, years=0.0), (0.0, 0.0, 0.0))
+    def test_turnover_sum_is_k2_weight(self):
+        '''
+        Sum of turnover is k2 * weight.
+        '''
+        weight = 1.0
+        turnover = turnover_builder(test_constants)
+        self.assertEqual(sum(turnover(weight=weight, years=1.0)), test_constants.k2 * weight)
+    def test_turnover_expose_default_rate_of_turnover(self):
+        '''
+        Exposes default rate of turnover.
+        
+        With default parameters, half of biomass is turned over each year.
+        '''
+        weight = 1.0
+        turnover = turnover_builder(test_constants)
+        self.assertEqual(sum(turnover(weight=weight, years=1.0)), weight/2)
+
+class TestErosionBuilder(unittest.TestCase):
+    '''
+    Tests the erosion_builder function.
+    '''
+    def test_erosion_builder_returns_callable(self):
+        '''
+        Tests the erosion_builder function returns a callable.
+        '''
+        self.assertTrue(callable(erosion_builder(test_constants)))
+
+class TestPartialErosion(unittest.TestCase):
+    '''Tests the partial erosion function.'''
+    def test_partial_erosion_returns_callable(self):
+        '''Tests the erosion_builder function returns a function.'''
+        self.assertTrue(callable(erosion_builder(test_constants)(lambda x: x)))
+    def test_partial_erosion_returns_callable_w_integration_fx(self):
+        '''Tests the erosion_builder function returns a function.'''
+        self.assertTrue(callable(erosion_builder(test_constants)(integration_builder(test_constants)(distribution_builder(test_constants)(1.0))))) # pylint: disable=line-too-long
+
+class TestErosion(unittest.TestCase):
+    '''Tests the erosion function.'''
+    # pylint: disable=line-too-long
+    def test_erosion_returns_tuple(self):
+        '''Tests the erosion function returns a tuple.'''
+        integration = integration_builder(test_constants)(distribution_builder(test_constants)(1.0))
+        erosion = erosion_builder(test_constants)(integration)
+        self.assertTrue(isinstance(erosion((0, 1), 1), tuple))
+    def test_erosion_negative_depth_raises_value_error(self):
+        '''Tests the erosion function raises ValueError with negative depth.'''
+        erosion = erosion_builder(test_constants)(integration_builder(test_constants)(distribution_builder(test_constants)(1.0)))
+        with self.assertRaises(ValueError):
+            erosion((-1.0, 1), 1.0)
+    def test_erosion_negative_weight_raises_value_error(self):
+        '''Tests the erosion function raises ValueError with negative weight.'''
+        erosion = erosion_builder(test_constants)(integration_builder(test_constants)(distribution_builder(test_constants)(1.0)))
+        with self.assertRaises(ValueError):
+            erosion((0, 1), -1.0)
+    def test_erosion_output_sums_to_biomass_increment_of_depth(self):
+        '''Tests the erosion function output sums to biomass weight in eroded depth.'''
+        integration = integration_builder(test_constants)(distribution_builder(test_constants)(1.0))
+        erosion = erosion_builder(test_constants)(integration)
+        self.assertAlmostEqual(sum(erosion((0, 1), erosion=1.0)), integration((0, 1)), 4)
+    def test_erosion_output_sums_to_biomass_in_depth(self):
+        '''Tests the erosion function output sums to biomass weight in eroded depth.'''
+        integration = integration_builder(test_constants)(distribution_builder(test_constants)(1.0))
+        erosion = erosion_builder(test_constants)(integration)
+        self.assertAlmostEqual(sum(erosion((0, 1), erosion=10)), integration((0, 10)), 4)
+
+class TestBurialBuilder(unittest.TestCase):
+    '''
+    Tests the burial_builder function.
+    '''
+    def test_burial_builder_returns_callable(self):
+        '''
+        Tests the burial_builder function returns a callable.
+        '''
+        self.assertTrue(callable(burial_builder(test_constants)))
+
+class TestPartialBurial(unittest.TestCase):
+    '''Tests the partial burial function.'''
+    def test_partial_burial_returns_callable(self):
+        '''Tests the burial_builder function returns a function.'''
+        self.assertTrue(callable(burial_builder(test_constants)(lambda x: x)))
+    def test_partial_burial_returns_callable_w_integration_fx(self):
+        '''Tests the burial_builder function returns a function.'''
+        self.assertTrue(callable(burial_builder(test_constants)(integration_builder(test_constants)(distribution_builder(test_constants)(1.0))))) # pylint: disable=line-too-long
+
+class TestBurial(unittest.TestCase):
+    '''Tests the burial function.'''
+    def test_burial_returns_tuple(self):
+        '''Tests the burial function returns a tuple.'''
+        integration = integration_builder(test_constants)(distribution_builder(test_constants)(1.0))
+        burial = burial_builder(test_constants)(integration)
+        self.assertTrue(isinstance(burial((0, 1), 1), tuple))
+    def test_burial_negative_depth_raises_value_error(self):
+        '''Tests the burial function raises ValueError with negative depth.'''
+        integration = integration_builder(test_constants)(distribution_builder(test_constants)(1.0))
+        burial = burial_builder(test_constants)(integration)
+        with self.assertRaises(ValueError):
+            burial((-1.0, 1), 1.0)
+    def test_burial_descending_depth_raises_value_error(self):
+        '''Tests the burial function raises ValueError with descending depth.'''
+        integration = integration_builder(test_constants)(distribution_builder(test_constants)(1.0))
+        burial = burial_builder(test_constants)(integration)
+        with self.assertRaises(ValueError):
+            burial((1, 0), 1.0)
+    def test_burial_negative_deposition_raises_value_error(self):
+        '''Tests the burial function raises ValueError with negative deposition.'''
+        integration = integration_builder(test_constants)(distribution_builder(test_constants)(1.0))
+        burial = burial_builder(test_constants)(integration)
+        with self.assertRaises(ValueError):
+            burial((0, 1), -1.0)
+    def test_burial_output_sums_to_biomass_increment_of_depth(self):
+        '''Tests the burial function output sums to biomass weight in deposited depth.'''
+        integration = integration_builder(test_constants)(distribution_builder(test_constants)(1.0))
+        expected = integration((test_constants.rd - 1, test_constants.rd))
+        burial = burial_builder(test_constants)(integration)
+        actual = burial((test_constants.rd - 1, test_constants.rd), deposition=2.0)
+        self.assertAlmostEqual(sum(actual), expected, 4)
